4d3345a3578d8c871f850772b3ff069166fbe6d4
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-08-16 11:22:48 +0500

    Script updated. Proxy option added. Now message can be sent to multiple users at once!

--- main.py
+++ main.py
@@ -4,28 +4,77 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from tqdm import tqdm
 import time
 import os
 import random
+import json
 import re
 
 
-def setup_driver(username, password):
-    service = Service(executable_path=os.path.join(os.getcwd(), "chromedriver.exe"))
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--start-maximized") 
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
-    chrome_options.add_experimental_option("useAutomationExtension", False) 
-    driver = webdriver.Chrome(service=service,options=chrome_options)
-    driver.get("https://www.facebook.com")
+def setup_driver(username, password, proxyAddr="", proxyPort=""):
 
-    email_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='email']")))
-    email_input.send_keys(username)
-    password_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='pass']")))
-    password_input.send_keys(password)
-    password_input.send_keys(Keys.RETURN)
+    cookies_exist = os.path.exists(os.path.join(os.getcwd(), "cookies",f"{username}.json"))
+
+    if proxyAddr != "" and proxyPort != "":
+        try:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument(f'--proxy-server={proxyAddr}:{proxyPort}')
+            chrome_options.add_argument("--start-maximized") 
+            chrome_options.add_argument("--disable-notifications")
+            chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+            chrome_options.add_experimental_option("useAutomationExtension", False) 
+            driver = webdriver.Chrome(executable_path=os.path.join(os.getcwd(), "chromedriver.exe"), options=chrome_options)
+            driver.get("https://www.facebook.com")
+        except:
+
+            print("Proxy not working, We will use default settings")
+
+            service = Service(executable_path=os.path.join(os.getcwd(), "chromedriver.exe"))
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--start-maximized") 
+            chrome_options.add_argument("--disable-notifications")
+            chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+            chrome_options.add_experimental_option("useAutomationExtension", False) 
+            driver = webdriver.Chrome(service=service,options=chrome_options)
+            driver.get("https://www.facebook.com")
+
+    else:
+        service = Service(executable_path=os.path.join(os.getcwd(), "chromedriver.exe"))
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--start-maximized") 
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+        chrome_options.add_experimental_option("useAutomationExtension", False) 
+        driver = webdriver.Chrome(service=service,options=chrome_options)
+        driver.get("https://www.facebook.com")
+    
+    if cookies_exist:
+        
+        try:
+            driver.get("https://facebook.com")
+            cookies = json.load(open(os.path.join(os.getcwd(), "cookies",f"{username}.json"), "r"))
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.get("https://facebook.com")
+            return driver
+        except: pass
+
+    else:
+
+        email_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='email']")))
+        email_input.send_keys(username)
+        password_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='pass']")))
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.RETURN)
+
+        time.sleep(20) # Time for 2FA authentication, feel free to remove it if you are using this bot with an account whch is not 2FA protected
+        
+        cookies = driver.get_cookies()
+        json.dump(cookies, open(os.path.join(os.getcwd(), "cookies",f"{username}.json"), "w"))
 
     time.sleep(random.randint(2,4))
     return driver
@@ -82,14 +131,35 @@ def send_message(driver, account_link, message):
     except Exception as e:
         pass
 
-def test_fn(username, password, account_link, message):
-    driver = setup_driver(username, password)
-    time.sleep(30) # for 2 FA :>
-    send_message(driver, account_link, message)
+def send_bulk_msgs(driver, account_links, message):
+    for account_link in tqdm(account_links, desc="Sending Messages"):
+        send_message(driver, account_link, message)
+
 
 if __name__ == "__main__":
     username = input("Username: ")
     password = input("Password: ")
-    account_link = input("Account Link: ")
-    message = input("Message: ")
-    test_fn(username, password, account_link, message)
+    
+    num = input("To how many accounts do you want to send message?")
+    try:
+        num = int(num)
+    except:
+        raise Exception("Please enter a valid number")
+
+    account_links = []
+    for i in range(num):
+        account_link = input("Enter account link: ")
+        account_links.append(account_link)
+
+    message = input("Enter message: ")
+    wanna_use_proxy = input("Do you want to use proxy? (y/n): ")
+    if wanna_use_proxy.lower() in ['y', 'yes']:
+        proxy = input("Enter proxy address: ")
+        proxy_port = input("Enter proxy port: ")
+        driver = setup_driver(username, password, proxy, proxy_port)
+    else:
+        driver = setup_driver(username, password)
+
+    time.sleep(20)
+
+    send_bulk_msgs(driver, account_links, message)
